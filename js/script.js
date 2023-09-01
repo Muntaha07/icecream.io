577401a99f7a8623f53fcf02530ad4ea69a34b98
Muntaha07 <beighmuntaha7@gmail.com>
2023-09-01 16:40:40 +0530

    yahoo

---
+++ js/script.js
@@ -0,0 +1,88 @@
+var timeout;
+
+let navbar = document.querySelector('.header .navbar');
+let menuBtn = document.querySelector('#menu-btn');
+
+menuBtn.onclick = () =>{
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menuBtn.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+
+var swiper = new Swiper(".home-slider", {
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true,
+    },
+    grabCursor:true,
+    loop:true,
+    spaceBetween:20,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+
+
+  
+function circleChaptaKaro() {
+    // define default scale value
+    var xscale = 1;
+    var yscale = 1;
+  
+    var xprev = 0;
+    var yprev = 0;
+  
+    window.addEventListener("mousemove", function (dets) {
+      clearTimeout(timeout);
+  
+      xscale = gsap.utils.clamp(0.8, 1.2, dets.clientX - xprev);
+      yscale = gsap.utils.clamp(0.8, 1.2, dets.clientY - yprev);
+  
+      xprev = dets.clientX;
+      yprev = dets.clientY;
+  
+      circleMouseFollower(xscale, yscale);
+  
+      timeout = setTimeout(function () {
+        document.querySelector(
+          "#minicircle"
+        ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+      }, 100);
+    });
+  }
+  
+  
+  function circleMouseFollower(xscale, yscale) {
+    window.addEventListener("mousemove", function (dets) {
+      document.querySelector(
+        "#minicircle"
+      ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
+    });
+  }
+  
+  circleChaptaKaro();
+  circleMouseFollower();
